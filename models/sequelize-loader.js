19d05a543b9f3857ac6e1fcb672a0da88fdd3029
netaro-N <takebbb1+github@gmail.com>
2019-02-09 03:09:21 +0000

    change database &add sort

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,7 +1,7 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  'postgres://postgres:postgres@localhost/schedule_arranger',
+  'postgres://postgres:postgres@localhost/schedule_arranger_4022',
   {
     operatorsAliases: false
   });
