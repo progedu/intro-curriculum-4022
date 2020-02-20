31e61d14d08a29d8b92d1947fd2e0b8414111a39
p-t-a-p-1 <kb240225+1@gmail.com>
2020-02-20 23:15:53 +0900

    [fix] 更新日時をUTCから日本時間へ表示修正

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,33 @@
-'use strict';
-const express = require('express');
-const router = express.Router();
-const Schedule = require('../models/schedule');
+'use strict'
+const express = require('express')
+const router = express.Router()
+const Schedule = require('../models/schedule')
+const moment = require('moment-timezone')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  const title = '予定調整くん';
+  const title = '予定調整くん'
   if (req.user) {
     Schedule.findAll({
       where: {
         createdBy: req.user.id
       },
       order: [['"updatedAt"', 'DESC']]
-    }).then((schedules) => {
+    }).then(schedules => {
+      schedules.forEach(schedule => {
+        schedule.formattedUpdatedAt = moment(schedule.updatedAt)
+          .tz('Asia/Tokyo')
+          .format('YYYY/MM/DD HH:mm')
+      })
       res.render('index', {
         title: title,
         user: req.user,
         schedules: schedules
-      });
-    });
+      })
+    })
   } else {
-    res.render('index', { title: title, user: req.user });
+    res.render('index', { title: title, user: req.user })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
