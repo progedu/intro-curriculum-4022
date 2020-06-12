353648d4d80e52522571d7a3b7036abe719800d9
ishinn <ishinn.naka@gmail.com>
2020-06-12 11:02:47 +0900

    日付をJSTに変換しYYYY/MM/DD hh:mm形式に変更

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
   const title = '予定調整くん';
   if (req.user) {
     Schedule.findAll({
-      where: {ｇ
+      where: {
         createdBy: req.user.id
       },
       order: [['updatedAt', 'DESC']]
