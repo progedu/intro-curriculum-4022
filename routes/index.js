719fb93c21dc5a4ce2c3e597b54105b4c8fa02b5
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-12-12 19:06:28 +0900

    トップページの更新日時を日本標準時刻に変更

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 'use strict';
 const express = require('express');
+const moment = require('moment-timezone');
+
 const router = express.Router();
 const Schedule = require('../models/schedule');
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -13,6 +16,10 @@ router.get('/', (req, res, next) => {
       },
       order: [['updatedAt', 'DESC']]
     }).then((schedules) => {
+      schedules.forEach(schedules => {
+        // formattedUpdatedAt プロパティを schedule に新たに作り、moment で updatedAt を形式変換する
+        schedules.formattedUpdatedAt = moment(schedules.updatedAt).tz('Asia/Tokyo').format('YYYY/MM/DD HH:mm');
+      });
       res.render('index', {
         title: title,
         user: req.user,
