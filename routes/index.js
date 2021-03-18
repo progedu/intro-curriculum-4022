d112f1f840967c9fff56a12246c7de1b1f427a10
Ryuta Watanabe <watanabe@initinc.co.jp>
2021-03-18 10:38:15 +0900

    トップページの更新日時を日本標準時に変更

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,32 @@
-'use strict';
-const express = require('express');
+"use strict";
+const express = require("express");
 const router = express.Router();
-const Schedule = require('../models/schedule');
+const Schedule = require("../models/schedule");
+const moment = require("moment-timezone");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  const title = '予定調整くん';
+router.get("/", (req, res, next) => {
+  const title = "予定調整くん";
   if (req.user) {
     Schedule.findAll({
       where: {
         createdBy: req.user.id
       },
-      order: [['updatedAt', 'DESC']]
+      order: [["updatedAt", "DESC"]]
     }).then((schedules) => {
-      res.render('index', {
+      schedules.forEach((schedule) => {
+        schedule.formattedUpdatedAt = moment(schedule.updatedAt)
+          .tz("Asia/Tokyo")
+          .format("YYYY/MM/DD HH:mm");
+      });
+      res.render("index", {
         title: title,
         user: req.user,
         schedules: schedules
       });
     });
   } else {
-    res.render('index', { title: title, user: req.user });
+    res.render("index", { title: title, user: req.user });
   }
 });
 
